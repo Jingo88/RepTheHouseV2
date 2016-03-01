f9ab01bdf3681608e680466b35a2be1b1a33338c
Jason <Jcory.ng@gmail.com>
2016-03-01 18:07:27 -0500

    scratch html form. how to do react form?

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,10 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./app"
-
+import Blah from "./app"
 
 ReactDOM.render(
 	<App name="Jason"/>, 
 	document.getElementById('home')
 )
-
